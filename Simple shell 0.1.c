7ceaf47b45c4a71ef876e922add5255ed2a6f531
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-03-06 01:02:42 -0600

    s

--- Simple shell 0.1.c
+++ Simple shell 0.1.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <string.h>
 
 #define MAX_COMMAND_LENGTH 1024
 
@@ -10,32 +10,30 @@ int main(void) {
     char command[MAX_COMMAND_LENGTH];
 
     while (1) {
-        printf("$ ");
+        printf("$ "); // Print prompt before input
         fflush(stdout);
 
         if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL) {
             printf("\n");
-            break;
+            break; // End of file (Ctrl+D) or error
         }
 
-        command[strlen(command) - 1] = '\0';
+        command[strlen(command) - 1] = '\0'; // Remove newline character
 
-        if (strcmp(command, "/bin/ls") == 0) {
-            pid_t pid = fork();
+        pid_t pid = fork();
 
-            if (pid < 0) {
-                perror("fork");
-                exit(EXIT_FAILURE);
-            } else if (pid == 0) {
-                execl("/bin/ls", "/bin/ls", NULL);
-                perror("execl");
-                exit(EXIT_FAILURE);
-            } else {
-                int status;
-                waitpid(pid, &status, 0);
-            }
+        if (pid < 0) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) {
+            // Child process
+            execlp(command, command, NULL);
+            perror("execlp");
+            exit(EXIT_FAILURE);
         } else {
-            printf("Command not supported: %s\n", command);
+            // Parent process
+            int status;
+            waitpid(pid, &status, 0);
         }
     }
 

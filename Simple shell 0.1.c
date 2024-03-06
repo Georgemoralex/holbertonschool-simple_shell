402d83598626b1b0478a7108974b9a816c866ce7
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-03-06 00:44:55 -0600

    s

--- Simple shell 0.1.c
+++ Simple shell 0.1.c
@@ -9,36 +9,27 @@ int main(void) {
     char command[MAX_COMMAND_LENGTH];
 
     while (1) {
-        // Display prompt
         printf("$ ");
         fflush(stdout);
 
-        // Read command from user
         if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL) {
             printf("\n");
-            break; // End of file (Ctrl+D) or error
+            break;
         }
 
-        // Remove newline character
         command[strlen(command) - 1] = '\0';
 
-        // Check if the command is "/bin/ls"
         if (strcmp(command, "/bin/ls") == 0) {
-            // Fork a child process
             pid_t pid = fork();
 
             if (pid < 0) {
                 perror("fork");
                 exit(EXIT_FAILURE);
             } else if (pid == 0) {
-                // Child process
                 execl("/bin/ls", "/bin/ls", NULL);
-                // If execl returns, an error occurred
                 perror("execl");
                 exit(EXIT_FAILURE);
             } else {
-                // Parent process
-                // Wait for the child process to finish
                 int status;
                 waitpid(pid, &status, 0);
             }

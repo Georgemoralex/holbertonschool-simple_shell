fc1296103662764f6ca89cb9e57ee06085b6efbd
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-03-14 02:20:09 -0600

    s

--- Simple shell 0.1.c
+++ Simple shell 0.1.c
@@ -16,49 +16,49 @@ void prompt() {
 #include <stdlib.h>
 
 void execute_command(char *command) {
-    pid_t pid = fork();
+	pid_t pid = fork();
 
-    if (pid == -1) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    } else if (pid == 0) {
-        char **argv = malloc(sizeof(char*) * 2);
-        if (argv == NULL) {
-            perror("malloc");
-            exit(EXIT_FAILURE);
-        }
-        argv[0] = command;
-        argv[1] = NULL;
+	if (pid == -1) {
+		perror("fork");
+		exit(EXIT_FAILURE);
+	} else if (pid == 0) {
+		char **argv = malloc(sizeof(char*) * 2);
+		if (argv == NULL) {
+			perror("malloc");
+			exit(EXIT_FAILURE);
+		}
+		argv[0] = command;
+		argv[1] = NULL;
 
-        extern char **environ;
-        execve(command, argv, environ);
-        perror("execve");
-        exit(EXIT_FAILURE);
-    } else {
-        int status;
-        waitpid(pid, &status, 0);
-    }
-    printf("\n");
+		extern char **environ;
+		execve(command, argv, environ);
+		perror("execve");
+		exit(EXIT_FAILURE);
+	} else {
+		int status;
+		waitpid(pid, &status, 0);
+	}
+	printf("\n");
 }
 
 int main() {
-    char command[MAX_COMMAND_LENGTH];
+	char command[MAX_COMMAND_LENGTH];
 
-    while (1) {
-        prompt();
-        if (fgets(command, sizeof(command), stdin) == NULL) {
-            printf("\n");
-            break;
-        }
+	while (1) {
+		prompt();
+		if (fgets(command, sizeof(command), stdin) == NULL) {
+			printf("\n");
+			break;
+		}
 
-        command[strcspn(command, "\n")] = '\0';
+		command[strcspn(command, "\n")] = '\0';
 
-        if (strlen(command) == 0) {
-            continue;
-        }
+		if (strlen(command) == 0) {
+			continue;
+		}
 
-        execute_command(command);
-    }
+		execute_command(command);
+	}
 
-    return 0;
+	return 0;
 }

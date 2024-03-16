41475327d9ad2aaa57e2858da4ed2ee2f99e6b78
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-03-16 13:15:54 -0600

    s

--- simple_shell.c
+++ simple_shell.c
@@ -1,31 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-#define MAX_INPUT_LENGTH 1024
+#define MAX_INPUT_LENGTH 100
 
 int main(void) {
-	char input[MAX_INPUT_LENGTH];
-	char *token;
+    char input[MAX_INPUT_LENGTH];
 
-	while (1) {
-		
-		printf("$ ");
-		fflush(stdout);
-		
-		if (fgets(input, sizeof(input), stdin) == NULL) {
-			printf("\nGoodbye!\n");
-			break;
-		}
+    while (1) {
 
-		input[strcspn(input, "\n")] = '\0';
+        printf("simple_shell> ");
+        fflush(stdout);
 
-		token = strtok(input, " ");
-		while (token != NULL) {
-			token = strtok(NULL, " ");
-		}
-	}
 
-	return 0;
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+
+            printf("\nGoodbye!\n");
+            break;
+        }
+
+        input[strlen(input) - 1] = '\0';
+
+      
+        pid_t child_pid = fork();
+        if (child_pid == 0) {
+
+            execlp(input, input, NULL); 
+            perror("Error");
+            exit(EXIT_FAILURE);
+        } else if (child_pid > 0) {
+
+            wait(NULL);
+        } else {
+            perror("Fork failed");
+        }
+    }
+
+    return 0;
 }

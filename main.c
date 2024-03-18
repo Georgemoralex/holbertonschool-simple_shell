460388b283e1789eeb31375a9429ab284ee4a33b
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-03-18 16:04:13 -0600

    s

--- main.c
+++ main.c
@@ -7,65 +7,54 @@
 
 #define MAX_COMMAND_LENGTH 1024
 
-/**
- * main - Entry point of the shell program
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-    char cmd[MAX_COMMAND_LENGTH];
-    pid_t pid;
-    ssize_t read_bytes;
+  char cmd[MAX_COMMAND_LENGTH];
+  pid_t pid;
+  ssize_t read_bytes;
+  int status;
 
-    while (1)
+  while (1)
+  {
+    if (isatty(STDIN_FILENO))
     {
-        if (isatty(STDIN_FILENO))
-        {
-            printf("$ ");
-            fflush(stdout);
-        }
-
-        memset(cmd, 0, sizeof(cmd));
-        read_bytes = read(STDIN_FILENO, cmd, MAX_COMMAND_LENGTH - 1);
-        if (read_bytes <= 0)
-        {
-            if (isatty(STDIN_FILENO))
-            {
-                printf("\n");
-            }
-            break;
-        }
-
-        cmd[read_bytes - 1] = '\0';
-
-        pid = fork();
-        if (pid == 0)
-        {
-            char *argv[4]; /* Command arguments array */
-            argv[0] = "/bin/sh";
-            argv[1] = "-c";
-            argv[2] = cmd;
-            argv[3] = NULL;
-
-            if (execvp(argv[0], argv) == -1)
-            {
-                fprintf(stderr, "%s: command not found\n", cmd);
-                exit(EXIT_FAILURE);
-            }
-        }
-        else if (pid > 0)
-        {
-            /* Parent process waits for the child to complete */
-            wait(NULL);
-        }
-        else
-        {
-            /* Handle fork failure */
-            perror("fork");
-            exit(EXIT_FAILURE);
-        }
+      printf("$ ");
+      fflush(stdout);
     }
 
-    return 0;
+    memset(cmd, 0, sizeof(cmd));
+    read_bytes = read(STDIN_FILENO, cmd, MAX_COMMAND_LENGTH - 1);
+    if (read_bytes <= 0)
+    {
+      if (isatty(STDIN_FILENO))
+      {
+        printf("\n");
+      }
+      break;
+    }
+
+    cmd[read_bytes - 1] = '\0';
+    pid = fork();
+    if (pid == 0)
+    {
+      char *argv[4] = {"/bin/sh", "-c", NULL, NULL};
+      argv[2] = cmd;
+      if (execvp(argv[0], argv) == -1)
+      {
+        fprintf(stderr, "./hsh: 1: %s: not found\n", cmd);
+        exit(127);
+      }
+    }
+    else if (pid > 0)
+    {
+      wait(&status);
+    }
+    else
+    {
+      perror("fork");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  return 0;
 }

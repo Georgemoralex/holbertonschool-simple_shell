1dcbd5f92d13aa964227ea4a1a0670ab647c74fc
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-03-18 15:39:55 -0600

    s

--- main.c
+++ main.c
@@ -7,42 +7,52 @@
 
 #define MAX_COMMAND_LENGTH 1024
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-	char cmd[MAX_COMMAND_LENGTH];
-	pid_t pid;
-	ssize_t read_bytes;
+    char cmd[MAX_COMMAND_LENGTH];
+    pid_t pid;
+    ssize_t read_bytes;
 
-	while (1) {
-		if (isatty(STDIN_FILENO))
-			printf("$ "), fflush(stdout);
+    while (1)
+    {
+        if (isatty(STDIN_FILENO))
+        {
+            printf("$ ");
+            fflush(stdout);
+        }
 
-		memset(cmd, 0, sizeof(cmd));
-		read_bytes = read(STDIN_FILENO, cmd, MAX_COMMAND_LENGTH - 1);
-		if (read_bytes <= 0) {
-			if (isatty(STDIN_FILENO))
-				printf("\n");
-			break;
-		}
+        memset(cmd, 0, sizeof(cmd));
+        read_bytes = read(STDIN_FILENO, cmd, MAX_COMMAND_LENGTH - 1);
+        if (read_bytes <= 0)
+        {
+            if (isatty(STDIN_FILENO))
+            {
+                printf("\n");
+            }
+            break;
+        }
 
-		cmd[read_bytes - 1] = '\0';
-		pid = fork();
-		if (pid == 0) {
-			char *argv[3] = {"sh", "-c", cmd};
-			execvp("/bin/sh", argv);
-			fprintf(stderr, "Failed to execute command\n");
-			exit(EXIT_FAILURE);
-		} else if (pid > 0) {
-			wait(NULL);
-		} else {
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-	}
-	return 0;
+        cmd[read_bytes - 1] = '\0';
+
+        pid = fork();
+        if (pid == 0)
+        {
+            char *argv[] = {"/bin/sh", "-c", cmd, NULL};
+
+            execvp(argv[0], argv);
+            perror("execvp");
+            exit(EXIT_FAILURE);
+        }
+        else if (pid > 0)
+        {
+            wait(NULL);
+        }
+        else
+        {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return 0;
 }

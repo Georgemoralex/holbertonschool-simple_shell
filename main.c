a4cc2710460753f020d739e322dc2ee65eb33dfa
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-03-18 02:23:11 -0600

    s

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ int main(void)
     {
         if (isatty(STDIN_FILENO))
         {
-            printf("$ ");
+            printf("");
         }
 
         if (fgets(cmd, sizeof(cmd), stdin) == NULL)

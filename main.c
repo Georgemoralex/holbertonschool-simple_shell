43becf87cf5b1cb3f220fb047d864c1b9512a2fd
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-03-18 00:06:31 -0600

    s

--- main.c
+++ main.c
@@ -13,7 +13,6 @@ void display_prompt() {
 
 int main() {
     char buffer[BUFFER_SIZE];
-    pid_t pid;
 
     while (1) {
         display_prompt();
@@ -25,24 +24,19 @@ int main() {
 
         buffer[strcspn(buffer, "\n")] = '\0';
 
-        pid = fork();
+        pid_t pid = fork();
 
         if (pid == -1) {
             perror("fork failed");
             exit(EXIT_FAILURE);
         } else if (pid == 0) {
             if (execlp(buffer, buffer, NULL) == -1) {
-                printf("Command not found: %s\n", buffer);
+                perror("execlp failed");
                 exit(EXIT_FAILURE);
             }
         } else {
             int status;
             waitpid(pid, &status, 0);
-            if (WIFEXITED(status) && WEXITSTATUS(status) == EXIT_SUCCESS) {
-                printf("Command executed successfully\n");
-            } else {
-                printf("Command failed\n");
-            }
         }
     }
 

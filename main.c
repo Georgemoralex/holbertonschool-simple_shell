0df22e3ca9171b3af750a0b279fc99a44479f221
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-03-18 16:12:07 -0600

    s

--- main.c
+++ main.c
@@ -7,54 +7,45 @@
 
 #define MAX_COMMAND_LENGTH 1024
 
-int main(void)
-{
-  char cmd[MAX_COMMAND_LENGTH];
-  pid_t pid;
-  ssize_t read_bytes;
-  int status;
+int main(void) {
+    char cmd[MAX_COMMAND_LENGTH];
+    pid_t pid;
+    ssize_t read_bytes;
 
-  while (1)
-  {
-    if (isatty(STDIN_FILENO))
-    {
-      printf("$ ");
-      fflush(stdout);
+    while (1) {
+        if (isatty(STDIN_FILENO)) {
+            printf("$ ");
+            fflush(stdout);
+        }
+
+        memset(cmd, 0, sizeof(cmd));
+        read_bytes = read(STDIN_FILENO, cmd, MAX_COMMAND_LENGTH - 1);
+        if (read_bytes <= 0) {
+            if (isatty(STDIN_FILENO)) {
+                printf("\n");
+            }
+            break;
+        }
+
+        cmd[read_bytes - 1] = '\0';
+
+        pid = fork();
+        if (pid == 0) {
+            char *argv[] = {"/bin/sh", "-c", cmd, NULL};
+
+            if (execvp(argv[0], argv) == -1) {
+                fprintf(stderr, "./hsh: 1: %s: not found\n", cmd);
+                exit(127);
+            }
+        } else if (pid > 0) {
+            int status;
+            wait(&status);
+        } else {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
     }
 
-    memset(cmd, 0, sizeof(cmd));
-    read_bytes = read(STDIN_FILENO, cmd, MAX_COMMAND_LENGTH - 1);
-    if (read_bytes <= 0)
-    {
-      if (isatty(STDIN_FILENO))
-      {
-        printf("\n");
-      }
-      break;
-    }
-
-    cmd[read_bytes - 1] = '\0';
-    pid = fork();
-    if (pid == 0)
-    {
-      char *argv[4] = {"/bin/sh", "-c", NULL, NULL};
-      argv[2] = cmd;
-      if (execvp(argv[0], argv) == -1)
-      {
-        fprintf(stderr, "./hsh: 1: %s: not found\n", cmd);
-        exit(127);
-      }
-    }
-    else if (pid > 0)
-    {
-      wait(&status);
-    }
-    else
-    {
-      perror("fork");
-      exit(EXIT_FAILURE);
-    }
-  }
-
-  return 0;
+    return 0;
 }
+
